2129342e0954431e8199dcaa5be8d62b7a5e26f2
Logan-Fouts <lf222yv@student.lnu.se>
2023-07-19 23:31:51 +0200

    UI functional

--- frontend/PicPurge/src/components/Options.tsx
+++ frontend/PicPurge/src/components/Options.tsx
@@ -1,16 +1,42 @@
+import React, { useState } from "react";
 import "./Options.css";
 
 function Options() {
+  const [folderPath, setFolderPath] = useState("");
+  const [aggressiveness, setAggressiveness] = useState("1");
+  const [removeNonMedia, setRemoveNonMedia] = useState(false);
+
+  const handleFolderPathChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setFolderPath(event.target.value);
+  };
+
+  const handleAggressivenessChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setAggressiveness(event.target.value);
+  };
+
+  const handleRemoveNonMediaChange = (event: { target: { checked: boolean | ((prevState: boolean) => boolean); }; }) => {
+    setRemoveNonMedia(event.target.checked);
+  };
+
+  const handleProcessClick = () => {
+    // Prepare the URL with the query parameters
+    const url = `http://localhost:5002/picAPI/run/${encodeURIComponent(folderPath)}/${aggressiveness}/${removeNonMedia}`;
+
+    // Make the GET request
+    fetch(url)
+    // TODO: Handle Response and Errors
+  };
+
   return (
     <div className="Options">
       <ul className="OptionList">
         <li className="OptionItem">
-          Select A Folder To Purge
-          <button className="FolderSelect">Files</button>
+          Enter Folder Path To Purge
+          <input type="text" className="FolderSelect" value={folderPath} onChange={handleFolderPathChange} />
         </li>
         <li className="OptionItem">
-          Set Agressivness
-          <select className="Agro">
+          Set Aggressiveness
+          <select className="Agro" value={aggressiveness} onChange={handleAggressivenessChange}>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -20,9 +46,11 @@ function Options() {
         </li>
         <li className="OptionItem">
           Remove Non-Media
-          <input type="checkbox" className="Checkbox" />
+          <input type="checkbox" className="Checkbox" checked={removeNonMedia} onChange={handleRemoveNonMediaChange} />
         </li>
-        <button className="Process">Process</button>
+        <button className="Process" onClick={handleProcessClick}>
+          Process
+        </button>
       </ul>
     </div>
   );

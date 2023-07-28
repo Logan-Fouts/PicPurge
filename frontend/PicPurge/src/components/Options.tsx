cb4b9ea9b100f7fb0a30273edcd3cf26c440f103
Logan-Fouts <lf222yv@student.lnu.se>
2023-07-28 22:35:55 +0200

    progress bar fix and general cleanup

--- frontend/PicPurge/src/components/Options.tsx
+++ frontend/PicPurge/src/components/Options.tsx
@@ -6,24 +6,32 @@ function Options() {
   const [aggressiveness, setAggressiveness] = useState("1");
   const [removeNonMedia, setRemoveNonMedia] = useState(false);
 
-  const handleFolderPathChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+  const handleFolderPathChange = (event: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
     setFolderPath(event.target.value);
   };
 
-  const handleAggressivenessChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+  const handleAggressivenessChange = (event: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
     setAggressiveness(event.target.value);
   };
 
-  const handleRemoveNonMediaChange = (event: { target: { checked: boolean | ((prevState: boolean) => boolean); }; }) => {
+  const handleRemoveNonMediaChange = (event: {
+    target: { checked: boolean | ((prevState: boolean) => boolean) };
+  }) => {
     setRemoveNonMedia(event.target.checked);
   };
 
   const handleProcessClick = () => {
     // Prepare the URL with the query parameters
-    const url = `http://localhost:5002/picAPI/run/${encodeURIComponent(folderPath)}/${aggressiveness}/${removeNonMedia}`;
+    const url = `http://localhost:5002/picAPI/run/${encodeURIComponent(
+      folderPath
+    )}/${aggressiveness}/${removeNonMedia}`;
 
     // Make the GET request
-    fetch(url)
+    fetch(url);
     // TODO: Handle Response and Errors
   };
 
@@ -32,11 +40,20 @@ function Options() {
       <ul className="OptionList">
         <li className="OptionItem">
           Enter Folder Path To Purge
-          <input type="text" className="FolderSelect" value={folderPath} onChange={handleFolderPathChange} />
+          <input
+            type="text"
+            className="FolderSelect"
+            value={folderPath}
+            onChange={handleFolderPathChange}
+          />
         </li>
         <li className="OptionItem">
           Set Aggressiveness
-          <select className="Agro" value={aggressiveness} onChange={handleAggressivenessChange}>
+          <select
+            className="Agro"
+            value={aggressiveness}
+            onChange={handleAggressivenessChange}
+          >
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -46,7 +63,12 @@ function Options() {
         </li>
         <li className="OptionItem">
           Remove Non-Media
-          <input type="checkbox" className="Checkbox" checked={removeNonMedia} onChange={handleRemoveNonMediaChange} />
+          <input
+            type="checkbox"
+            className="Checkbox"
+            checked={removeNonMedia}
+            onChange={handleRemoveNonMediaChange}
+          />
         </li>
         <button className="Process" onClick={handleProcessClick}>
           Process

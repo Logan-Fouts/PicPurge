1d71b73a15dbea682d958eb4ec5ee2c233ee632e
Will Lauer <willlauer4@gmail.com>
2023-08-08 13:37:09 -0400

    Progress Bar progress

--- frontend/PicPurge/src/App.tsx
+++ frontend/PicPurge/src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Banner from "./components/Banner.tsx";
 import Options from "./components/Options.tsx";
+import Progress from "./components/Progress.tsx";
 
 function App() {
   return (
     <>
       <Banner />
       <Options />
+      <Progress />
     </>
   );
 }

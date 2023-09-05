96bb62149daf280f6aba034dba10a3235cbfd427
Logan-Fouts <loganflogan@hotmail.com>
2023-09-05 23:28:27 +0200

    SUPERSANE SPEEEEED

--- Electron/picpurger.py
+++ Electron/picpurger.py
@@ -1,141 +1,104 @@
 import argparse
-import concurrent.futures
-import os
-import shutil
+import math
 import sys
-import threading
-import PIL
-from flask import Flask
-from tqdm import tqdm
+import os
+import glob
+from PIL import Image
 import imagehash
+import shutil
+from multiprocessing import Pool, Manager
 
-class ImageProcessor:
-    def __init__(self):
-        self.num_images = -1
-        self.lock = threading.Lock()
-        self.pbar = None
-        self.batch_size = 500
+def find_image_files(image_dir, image_extensions):
+    image_files = []
 
-    def compare_images(self, image1_path, image2_path, agro, pbar):
+    for root, _, files in os.walk(image_dir):
+        if duplicate_folder in root:
+            continue
+        for file in files:
+            if file.lower().endswith(tuple(image_extensions)):
+                image_files.append(os.path.join(root, file))
+
+    return image_files
+
+def compute_image_hash(image_file):
+    try:
+        img = Image.open(image_file)
+        img_hash = str(imagehash.dhash(img))
+        return image_file, img_hash
+    except Exception as e:
+        print(f"An error occurred for {image_file}: {e}")
+        return None
+
+def compute_image_hashes(image_dir, duplicate_folder, agro_threshold, batch_size=float(math.inf), progress=None):
+    image_files = find_image_files(image_dir, image_extensions)
+
+    hashes = {}
+    batch_hashes = []
+
+    total_images = len(image_files)
+
+    with Pool(num_workers) as pool:
         try:
-            hash1 = imagehash.dhash(PIL.Image.open(image1_path))
-            hash2 = imagehash.dhash(PIL.Image.open(image2_path))
-            hamming_distance = hash1 - hash2
-            result = hamming_distance <= agro
-            with self.lock:
+            for result in pool.imap(compute_image_hash, image_files):
                 if result:
-                    self.num_images -= 1
-                    pbar.update(self.num_images)
-                else:
-                    pbar.update(1)
+                    image_file, img_hash = result
+                    batch_hashes.append((image_file, img_hash))
+                    progress.value += 1  # Update progress
 
-            progress_percentage = self.get_progress_update()
-            print(f"Progress: {progress_percentage:.2f}%")
-            
-            return result
-        except (FileNotFoundError, PIL.UnidentifiedImageError) as e:
-            return False
+                    if len(batch_hashes) >= batch_size:
+                        compare_hashes(batch_hashes, hashes, duplicate_folder, image_files, agro_threshold)
+                        batch_hashes = []
 
-    def process_image_batch(self, image_paths, output_folder, agro, keep_non_media, pbar):
-        for i in range(len(image_paths)):
-            for j in range(i + 1, len(image_paths)):
-                image1_path = image_paths[i]
-                image2_path = image_paths[j]
-                self.process_image_pair(
-                    image1_path, image2_path, output_folder, agro, keep_non_media, pbar
-                )
+            compare_hashes(batch_hashes, hashes, duplicate_folder, image_files, agro_threshold)
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
-    def process_image_pair(
-        self, image1_path, image2_path, output_folder, agro, keep_non_media, pbar
-    ):
-        _, file_extension = os.path.splitext(image1_path)
+    return hashes
 
-        if image1_path == image2_path:
-            return
 
-        if file_extension.lower() not in IMAGE_EXTENSIONS + VIDEO_EXTENSIONS:
-            if not keep_non_media:
-                try:
-                    os.remove(image1_path)
-                except FileNotFoundError:
-                    pass
-            return
+def hamming_distance(hash1, hash2):
+    return bin(int(hash1, 16) ^ int(hash2, 16)).count('1')
 
-        _, file_extension = os.path.splitext(image2_path)
-        if file_extension.lower() not in IMAGE_EXTENSIONS + VIDEO_EXTENSIONS:
-            if not keep_non_media:
-                try:
-                    os.remove(image2_path)
-                except FileNotFoundError:
-                    pass
-            return
+def compare_hashes(batch_hashes, all_hashes, duplicate_folder, image_files, agro_threshold):
+    total_images = len(image_files)
+    processed_images = len(all_hashes)
+    progress_percentage = ((processed_images / total_images) * 100) + 50
 
-        if not os.path.exists(image1_path) or not os.path.exists(image2_path):
-            return
+    sys.stdout.flush()
+    print(f"Progress: {progress_percentage:.2f}%")
 
-        if self.compare_images(image1_path, image2_path, agro, pbar):
+    for i, (file, hash_val) in enumerate(batch_hashes, start=1):
+        duplicates = []
+        for existing_file, existing_hash in all_hashes.items():
+            if hamming_distance(hash_val, existing_hash) <= agro_threshold:
+                duplicates.append(existing_file)
+        all_hashes[file] = hash_val
+        if duplicates:
+            move_duplicates(file, duplicates, duplicate_folder, image_files)
+
+        progress_percentage = (((processed_images + i) / total_images) * 100) + 50
+        sys.stdout.flush()
+        print(f"Progress: {progress_percentage:.2f}%")
+
+def move_duplicates(file, duplicates, duplicate_folder, image_files):
+    if not os.path.exists(duplicate_folder):
+        os.makedirs(duplicate_folder)
+
+    for duplicate_file in duplicates:
+        if os.path.exists(duplicate_file):
             try:
-                # print(f"Duplicate Found")
+                shutil.move(duplicate_file, os.path.join(duplicate_folder, os.path.basename(duplicate_file)))
+
+                sys.stdout.flush()
                 print("Duplicate_Found_Message")  # Print the message immediately
-                sys.stdout.flush()  # Flush the standard output
-                shutil.move(
-                    image2_path, os.path.join(output_folder, os.path.basename(image2_path))
-                )
-            except FileNotFoundError:
-                pass
+                
+                if duplicate_file in image_files:
+                    image_files.remove(duplicate_file)
+            except Exception as e:
+                print(f"An error occurred while moving {duplicate_file}: {e}")
 
-    def init(self, folder_path, agro_threshold, keep_non_media):
-        self.image_paths = []
-        for root, dirs, files in os.walk(folder_path):
-            if "Duplicate-Images" in dirs:
-                dirs.remove("Duplicate-Images")
-
-            self.image_paths.extend([os.path.join(root, file) for file in files])
-
-        self.num_images = len(self.image_paths)
-        total_comparisons = (len(self.image_paths) * (len(self.image_paths) - 1)) // 2
-        self.pbar = tqdm(total=total_comparisons, desc="Progress", unit="comparisons")
-
-        output_folder = os.path.join(folder_path, "Duplicate-Images")
-        os.makedirs(output_folder, exist_ok=True)
-
-        image_batches = [
-            self.image_paths[i : i + self.batch_size] for i in range(0, len(self.image_paths), self.batch_size)
-        ]
-
-        # Begin Processing
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            futures = []
-            for image_batch in image_batches:
-                futures.append(
-                    executor.submit(
-                        self.process_image_batch,
-                        image_batch,
-                        output_folder,
-                        agro_threshold,
-                        keep_non_media,
-                        self.pbar,
-                    )
-                )
-
-            for future in concurrent.futures.as_completed(futures):
-                future.result()
-
-        self.pbar.close()
-        return self.pbar
-
-    def get_progress_update(self):
-        if self.pbar is not None and self.pbar.total != 0:
-            progress_percentage = (self.pbar.n / self.pbar.total) * 100
-            if progress_percentage > 100: return 100
-            return progress_percentage
-        else:
-            return 0
-
-app = Flask(__name__)
-
-IMAGE_EXTENSIONS = [".jpg", ".jpeg", ".png", ".gif", ".bmp"]
-VIDEO_EXTENSIONS = [".mp4", ".aavi", ".mkv", ".mov"]
+image_extensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp"]
+num_workers = 20
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Find and remove duplicate images.")
@@ -144,5 +107,8 @@ if __name__ == "__main__":
     parser.add_argument("--keep_non_media", action="store_true", help="Keep non-media files.")
     args = parser.parse_args()
 
-    processor = ImageProcessor()
-    processor.init(args.folder_path, args.agro_threshold, args.keep_non_media)
+    duplicate_folder = args.folder_path + "/Duplicate Images"
+
+    with Manager() as manager:
+        progress = manager.Value("i", 0)
+        hashes = compute_image_hashes(args.folder_path, duplicate_folder, args.agro_threshold, progress=progress)
